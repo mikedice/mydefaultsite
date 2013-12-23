94aac42ac8f58950fcacea7b22d53e327aae4b08
mikedice417 <mikedice417@hotmail.com>
2013-12-22 21:10:41 -0800

    gallery rotator

--- scripts/Site/sitecontroller.js
+++ scripts/Site/sitecontroller.js
@@ -1,2 +1,52 @@
 // sitecontroller.js
 // copyright 2014, mikedice417@hotmail.com
+
+var SiteController = (function(){
+
+    var currentGalleryImageList = null;
+    
+    // public interface
+    return {
+        Initialize: function(){
+            
+            // Load the gallery list
+            $.getJSON("images/galleryIndex.json")
+            .done(function(result){
+                var list = result;
+                SiteView.Initialize(list);        
+            }).error(function(result){
+                
+            });
+        },
+        ShowGallery: function(galleryJsonUrl)
+        {
+            // download gallery JSON
+            $.getJSON(galleryJsonUrl)
+                .done(function(result){
+
+                currentGalleryImageList = result;
+
+                // download partial view for gallery and tell the site view
+                // to switch the splash screen
+                $.ajax({
+                    url: "gallery-partial.html"
+                }).done(function(result){
+                    SiteView.SetGalleryView(result, currentGalleryImageList);    
+                }).error(function(result){
+                    // todo: handle error            
+                });
+
+            }).error(function(result){
+                // todo: handle error            
+            });
+        }
+    };
+}());
+
+
+// ViewController will load data, populate the model 
+// and render the view  after the doc is loaded.
+$(document).ready(function(){
+    
+    SiteController.Initialize();
+});
